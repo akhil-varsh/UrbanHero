6e377aedfc7c15c9f0bee7602d649bc8fc0c888e
Amruth-varsh <akvarsh1013@gmail.com>
2025-06-04 17:33:16 +0530

    upgrade: lib

--- lib/utils/dynamic.dart
+++
@@ -1,7 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-Future<void> saveUserData(String username, String email) async {
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.setString('username', username);
-  await prefs.setString('email', email);
-}
